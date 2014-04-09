2f41ae225a4e35b9eb58794ab58efa8671657c7e
Mark Wardle <mark@loganwebdesign.com>
2014-04-09 00:48:56 -0600

    Changed the top nav bar

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -14,24 +14,28 @@
                 </a>
             </h1>
         </li>
-        <li class="toggle-topbar menu-icon"><a href="#">Menu</a></li>
     </ul>
 
     <section class="top-bar-section">
         <!-- Right Nav Section -->
         <ul class="right">
-            <li class="active"><a href="#">Right Button Active</a></li>
+            <li class="has-form">
+                <div class="row collapse">
+                    <div class="large-8 small-9 columns">
+                        <input type="text" placeholder="Find Stuff">
+                    </div>
+                    <div class="large-4 small-3 columns">
+                        <a href="#" class="alert button expand">Search</a>
+                    </div>
+                </div>
+            </li>
             <li class="has-dropdown">
-                <a href="#">Right Button Dropdown</a>
+                <a href="#">User Name</a>
                 <ul class="dropdown">
-                    <li><a href="#">First link in dropdown</a></li>
+                    <li><a href="#">Logout</a></li>
+                    <li><a href="#">Settings</a></li>
                 </ul>
             </li>
-        </ul>
-
-        <!-- Left Nav Section -->
-        <ul class="left">
-            <li><a href="#">Left Nav Button</a></li>
         </ul>
     </section>
 </nav>
