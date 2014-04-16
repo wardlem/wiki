863bdae97bf317b72fc8e5c68a353d118741c8e3
Mark Wardle <mark@loganwebdesign.com>
2014-04-15 23:52:21 -0600

    Fixed the routes to check for the page_type table.

--- app/routes.php
+++ app/routes.php
@@ -44,15 +44,18 @@ Route::get('/', array('as' => 'home', 'uses' => 'PageController@homePage'))
 |
 */
 
-$pageTypes = PageType::all()->each(function($type){
-    Route::get($type->url_prefix . '/{page}/{tab?}', array('as' => $type->url_prefix, 'uses' => 'PageController@page'))
-        ->before('auth')
-        ->where('tab', '(content|revisions|discussion|edit)');
-});
+if (Schema::hasTable('page_types')){
+    $pageTypes = PageType::all()->each(function($type){
+        Route::get($type->url_prefix . '/{page}/{tab?}', array('as' => $type->url_prefix, 'uses' => 'PageController@page'))
+            ->before('auth')
+            ->where('tab', '(content|revisions|discussion|edit)');
+    });
 
-Route::group(array('prefix' => 'page', 'before' => 'auth'), function(){
-    Route::put('{page}', array('as' => 'update_page', 'uses' => 'PageController@updatePage'));
-});
+    Route::group(array('prefix' => 'page', 'before' => 'auth'), function(){
+        Route::put('{page}', array('as' => 'update_page', 'uses' => 'PageController@updatePage'));
+    });
+}
+
 
 
 
